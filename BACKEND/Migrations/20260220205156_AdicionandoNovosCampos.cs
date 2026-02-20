714692cfd21c6537196c57624919aa2466bb3233
Tiaguinho2002 <titi.zidoro18@gmail.com>
2026-02-20 17:52:09 -0300

    feat: atualizando migrations do banco

---
+++ BACKEND/Migrations/20260220205156_AdicionandoNovosCampos.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace SmartSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionandoNovosCampos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SystemInformations");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SystemInformations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Email = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    Nome = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    PasswordHash = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SystemInformations", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+    }
+}
